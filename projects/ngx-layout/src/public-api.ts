a48db28820de5cfd3aa39a0024dad627ee6f0ae1
HariharanT99 <hariharant99@gmail.com>
2021-12-19 21:12:35 +0530

    initial library

---
+++ projects/ngx-layout/src/public-api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of ngx-layout
+ */
+
+
+export * from './lib/ngx-layout.module';
+
+//Components
+
+export * from './lib/header/header.component';
+export * from './lib/footer/footer.component';
